4a3c2fd74dd9c2696b1dd33ea33fb070f246d464
mateusf-prog <mateus102006@hotmail.com>
2023-07-18 18:17:00 -0300

    created controller and model

---
+++ src/main/java/br/com/mateus/apirest/controller/ClientController.java
@@ -0,0 +1,12 @@
+package br.com.mateus.apirest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+    
+
+}
