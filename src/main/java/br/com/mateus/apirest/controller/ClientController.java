9347606742afa93cd2a4ab59b17e3c637217b2bc
mateusf-prog <mateus102006@hotmail.com>
2023-07-18 19:05:30 -0300

    get and post mapping in the controller

--- src/main/java/br/com/mateus/apirest/controller/ClientController.java
+++ src/main/java/br/com/mateus/apirest/controller/ClientController.java
@@ -1,15 +1,35 @@
 package br.com.mateus.apirest.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import br.com.mateus.apirest.model.Client;
+import br.com.mateus.apirest.repository.ClientRepository;
 
 @RestController
 @RequestMapping("/")
 public class ClientController {
     
+    @Autowired
+    private ClientRepository clientRepository;
 
-    
+    @GetMapping
+    public List<Client> listAll() {
+        return clientRepository.findAll();
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client save(@RequestBody Client obj) {
+        return clientRepository.save(obj);
+    }
 }
 
--- src/main/java/br/com/mateus/apirest/repository/ClientRepository.java
+++ src/main/java/br/com/mateus/apirest/repository/ClientRepository.java
@@ -1,9 +1,11 @@
 package br.com.mateus.apirest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.mateus.apirest.model.Client;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
     
 }
